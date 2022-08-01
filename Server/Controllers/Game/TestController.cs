3c0a761cb20fb566d491274c7dbe5e2fdb300726
newu2012 <newu2011@gmail.com>
2022-08-01 06:24:25 +0500

    add dummy controller for db testing

---
+++ Server/Controllers/Game/TestController.cs
@@ -0,0 +1,21 @@
+﻿using DataContext.Postgresql;
+using EntityModels.Postgresql;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers.Game;
+
+public class TestController : ControllerBase
+{
+    private MHIdleContext _db;
+
+    public TestController(MHIdleContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public Region[] Index()
+    {
+        return _db.Regions.ToArray();
+    }
+}
