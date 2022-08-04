23f37beb8a2fdcb1d2cdc80ece7f0748365615ef
newu2012 <newu2011@gmail.com>
2022-08-04 12:31:52 +0500

    sketch out TheMainMenuPanel and TheMainPanel

---
+++ Server/client/src/components/TheMainMenuPanel.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="TheMainMenuPanel">
+    <ul>
+      <li>Region</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.TheMainMenuPanel {
+  flex-basis: 320px;
+  height: 100%;
+  background-color: rgb(60, 63, 65);
+}
+</style>
---
+++ Server/client/src/components/TheMainPanel.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="TheMainPanel">
+    <h1>Something</h1>
+  </div>
+</template>
+
+<style scoped>
+.TheMainPanel {
+  flex-grow: 1;
+  text-align: center;
+  height: 100%;
+  background-color: rgb(43, 43, 43);
+}
+</style>
