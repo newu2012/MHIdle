4a886c6a18ad41f32c2232df14a1153627bd87e4
newu2012 <newu2011@gmail.com>
2022-08-05 06:05:08 +0500

    set linter to stricter mode, add prettier

--- Server/client/.eslintrc.cjs
+++ Server/client/.eslintrc.cjs
@@ -1,10 +1,9 @@
-/* eslint-env node */
 require("@rushstack/eslint-patch/modern-module-resolution")
 
 module.exports = {
   extends: [
     'eslint:recommended',
-    'plugin:vue/vue3-essential',
-    '@vue/eslint-config-typescript'
+    'plugin:vue/vue3-strongly-recommended',
+    '@vue/eslint-config-typescript',
   ]
 }
