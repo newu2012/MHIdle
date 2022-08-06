f4216c3987a1448b7d51afdab0e2ee5a635e4068
newu2012 <newu2011@gmail.com>
2022-08-06 20:57:12 +0500

    remove herb counter in TheRegionPanel

--- Server/client/src/components/TheRegionPanel.vue
+++ Server/client/src/components/TheRegionPanel.vue
@@ -1,20 +1,13 @@
 <script lang="ts" setup>
 
-import { ref } from "vue";
-import { Character } from "../models/character/Character";
 import { RegionService } from "../services/RegionService";
 import TYPES from "../types";
 import container from "../inversify.config";
 
-const character: Character = container.get<Character>(TYPES.Character);
 const regionService: RegionService = container.get<RegionService>(TYPES.RegionService);
-
-const herbsInInventory = ref(character.currentInventory
-  .GetItemOrEmptyItemStack("simpleHerb"));
 
 function Gather() {
   regionService.GatherHerbs();
-  console.log(herbsInInventory);
 }
 </script>
 
@@ -23,7 +16,6 @@ function Gather() {
   <button @click="Gather">
     Gather Herbs
   </button>
-  <h4>Herbs in inventory: {{ herbsInInventory.quantity }}</h4>
 </template>
 
 <style scoped>
