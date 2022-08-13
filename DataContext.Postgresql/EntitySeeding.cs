562919e097cfaee9e5cc75438b858720e9b7382c
newu2012 <newu2011@gmail.com>
2022-08-14 01:04:58 +0500

    Add EntitySeeding functionality

---
+++ DataContext.Postgresql/EntitySeeding.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace DataContext.Postgresql;
+
+public static class EntitySeeding
+{
+    public static IEnumerable<T> Seeding<T>(string? fileToParse = null)
+    {
+        //  TODO find how set relative path in this
+        var seedsFilePath = Path.Join("Seeds", $"{typeof(T).Name}.json");
+        
+        if (File.Exists(fileToParse ?? seedsFilePath))
+        {
+            var jsonText = File.ReadAllText(fileToParse ?? seedsFilePath);
+            var classObjects = JsonConvert.DeserializeObject<IEnumerable<T>>(jsonText);
+
+            return classObjects ?? Array.Empty<T>();
+        }
+
+        return Array.Empty<T>();
+    }
+}
---
+++ Server/Controllers/Admin/AdminController.cs
@@ -0,0 +1,45 @@
+﻿using DataContext.Postgresql;
+using EntityModels.Postgresql;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers.Admin;
+
+[ApiController]
+[Route("/api/admin")]
+public class AdminController : ControllerBase
+{
+    private MHIdleContext _db;
+
+    public AdminController(MHIdleContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public async Task UpdateSeeds()
+    {
+        //  TODO Change to HttpPut with line below
+        //  TODO Add file and name parameter for individual reseeding with custom files through API
+        Reseed();
+        await _db.SaveChangesAsync();
+
+        return;
+    }
+
+    private void Reseed()
+    {
+        var newResources = EntitySeeding.Seeding<Resource>().ToArray();
+        //  TODO Delete items that are not in Seed file 
+        for (var i = 0; i < newResources.Length; i++)
+        {
+            if (_db.Resources.Any(r => r.ResourceId == newResources[i].ResourceId))
+            {
+                _db.Resources.Update(newResources[i]);
+            }
+            else
+            {
+                _db.Resources.Add(newResources[i]);
+            }
+        }
+    }
+}
